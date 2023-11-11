56200367336e009a19c0d13cc3a18fa46a1a689f
swwapnil7755 <145371245+swwapnil7755@users.noreply.github.com>
2023-11-12 01:42:46 +0530

    Create variables.tf

---
+++ eks-cluster/variables.tf
@@ -0,0 +1,11 @@
+variable "private_subnet_cidr_blocks" {
+    type = list(string)
+}
+
+variable "public_subnet_cidr_blocks" {
+    type = list(string)
+}
+
+variable "vpc_cidr_block" {
+  type = string
+}
