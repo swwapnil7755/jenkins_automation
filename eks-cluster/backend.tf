aa08aeb4efe13d40b2f984f57f57729f5c20f2b1
swwapnil7755 <145371245+swwapnil7755@users.noreply.github.com>
2023-11-12 01:56:23 +0530

    Update backend.tf

--- eks-cluster/backend.tf
+++ eks-cluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "cicd-automation"
+        bucket = "cicd-automation1212"
         region = "us-east-1"
         key    = "eks/terraform.tfstate"
     }
